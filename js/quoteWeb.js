62e2476b101335a2e659c618783145053fb846f0
Maliha Kabir <43598622+MalihaKabir@users.noreply.github.com>
2019-01-31 00:33:56 +0600

    Update quoteWeb.js

--- js/quoteWeb.js
+++ js/quoteWeb.js
@@ -11,7 +11,7 @@ let bgColor = document.querySelector('#bgColor')
 
 switch (day) {
     case 0:
-      quote.innerHTML = `"Believe you can and you're halfway there."`
+      quote.innerHTML = `Believe you can and you're halfway there.`
       footer.innerHTML = `Theodore Roosevelt`
       date.innerHTML = today
       greeting.innerHTML = `Let's begin the week with the believe. Happy <strong>Sunday</strong>!`
@@ -25,7 +25,7 @@ switch (day) {
       bgColor.style.background = 'linear-gradient(to right, rgb(255, 255, 0), rgb(255, 255, 255))'
       break
     case 2:
-      quote.innerHTML = `"Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it. And, like any great relationship, it just gets better and better as the years roll on."`
+      quote.innerHTML = `Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it. And, like any great relationship, it just gets better and better as the years roll on."`
       footer.innerHTML = `Steve Jobs`
       greeting.innerHTML = `Have a great <strong>Tuesday</strong>!`
       date.innerHTML = today
