07ddad0480bdbc33e7bc763078a37b219dd5a667
Maliha Kabir <43598622+MalihaKabir@users.noreply.github.com>
2019-02-25 00:11:22 +0600

    Update particlesApp.js

--- js/particlesApp.js
+++ js/particlesApp.js
@@ -66,7 +66,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 5,
         "direction": "none",
         "random": false,
         "straight": false,
